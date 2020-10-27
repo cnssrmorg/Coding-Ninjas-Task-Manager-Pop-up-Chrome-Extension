8c31468b74a74d2d2c9600745a49d93ecb7da364
abhinavmaharana <abhinavmaharana.004@gmail.com>
2020-10-27 13:02:21 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 import TaskListContextProvider from './context/TaskListContext';
 
-
 function App() {
   return (
     <TaskListContextProvider>
-      <div className="container">
+      <div className="container" onMouseMove={this._dragStart} onMouseMove={this._dragging} onMouseUp={this._dragEnd}>
         <div className="app-wrapper">
           <div className="main">
             <Header />
